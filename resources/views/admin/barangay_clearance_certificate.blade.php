41e86b513b8c1e88c678bb4f98e2473d0d96092e
Alyza Rose Morada <zebgutierrez20@gmail.com>
2024-07-27 17:56:23 +0800

    tnx

--- resources/views/admin/barangay_clearance_certificate.blade.php
+++ resources/views/admin/barangay_clearance_certificate.blade.php
@@ -13,7 +13,7 @@
                     <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Dashboard</a></li>
-                            <li class="breadcrumb-item active">Brgy. Clearance Certificate</li>
+                            <li class="breadcrumb-item active">Brgy. Clearance Certificate Management</li>
                         </ol>
                     </div>
                 </div>
