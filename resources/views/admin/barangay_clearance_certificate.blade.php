cbe03c517c9cb5a2121317de0b2b35bc3b0a3edf
Alyza Rose Morada <zebgutierrez20@gmail.com>
2024-07-27 21:33:17 +0800

    tnx

--- resources/views/admin/barangay_clearance_certificate.blade.php
+++ resources/views/admin/barangay_clearance_certificate.blade.php
@@ -7,9 +7,11 @@
         <section class="content-header">
             <div class="container-fluid">
                 <div class="row mb-2">
-                    <div class="col-sm-6"></div>
                     <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
+                        
+                    </div>
+                    <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Dashboard</a></li>
                             <li class="breadcrumb-item active">Brgy. Clearance Certificate Management</li>
                         </ol>
@@ -53,6 +55,7 @@
         </section>
     </div>
     
+
     <!-- Add Clearance Certificate Modal Start -->
     <div class="modal fade" id="modalAddBarangayClearanceCertificate" tabindex="-1" data-bs-keyboard="false" data-bs-backdrop="static">
         <div class="modal-dialog modal-lg">
@@ -68,10 +71,10 @@
                             <div class="col-md-12">
                                 <div class="card-body">
                                     <!-- For Edit -->
-                                    <input type="hidden" name="barangay_clearance_certificate_id">
+                                    <input type="text" class="form-control" style="display: none" name="barangay_clearance_certificate_id" aria-label="Default" aria-describedby="inputGroup-sizing-default">
 
                                     <!-- Id of Request Cedula Basis -->
-                                    <input type="hidden" name="issuance_configuration_id" id="issuanceConfigurationId">
+                                    <input type="text" class="form-control" style="display: none" name="issuance_configuration_id" id="issuanceConfigurationId" placeholder="Issuance Certificate Id">
 
                                     <div class="mb-3">
                                         <label for="selectResident" class="form-label">Select Resident<span class="text-danger" title="Required">*</span></label>
@@ -99,23 +102,23 @@
                                     
                                     <div class="mb-3">
                                         <label for="textPurpose" class="form-label">Purpose<span class="text-danger" title="Required">*</span></label>
-                                        <textarea class="form-control" rows="3" name="purpose" id="textPurpose" placeholder="Purpose"></textarea>
+                                        <textarea type="text" class="form-control" rows="3" name="purpose" id="textPurpose" placeholder="Purpose"></textarea>
                                     </div>
                                     <div class="mb-3">
                                         <label for="textORNumber" class="form-label">OR Number<span class="text-danger" title="Required">*</span></label>
-                                        <input type="text" class="form-control" name="or_number" id="textORNumber" placeholder="OR Number">
+                                        <input type="text" class="form-control" rows="4" name="or_number" id="textORNumber" placeholder="OR Number">
                                     </div>
                                     <div class="mb-3">
                                         <label for="textAmountCollection" class="form-label">Amount Collection<span class="text-danger" title="Required">*</span></label>
-                                        <input type="text" class="form-control" name="amount_collection" id="textAmountCollection" placeholder="Amount Collection">
+                                        <input type="text" class="form-control" rows="4" name="amount_collection" id="textAmountCollection" placeholder="Amount Collection">
                                     </div>
                                     <div class="mb-3">
                                         <label for="textIssuedOn" class="form-label">Issued On<span class="text-danger" title="Required">*</span></label>
-                                        <input type="date" class="form-control" name="issued_on" id="textIssuedOn">
+                                        <input type="date" class="form-control" rows="4" name="issued_on" id="textIssuedOn" placeholder="Issued On">
                                     </div>
                                     <div class="mb-3">
                                         <label for="textRemarks" class="form-label">Remarks</label>
-                                        <textarea class="form-control" rows="3" name="remarks" id="textRemarks" placeholder="Remarks"></textarea>
+                                        <textarea type="text" class="form-control" rows="3" name="remarks" id="textRemarks" placeholder="Remarks"></textarea>
                                     </div>
                                     <div class="mb-3">
                                         <label for="textStatus" class="form-label">Status<span class="text-danger" title="Required">*</span></label>
@@ -124,7 +127,7 @@
                                             <option value="1">Issued</option>
                                             <option value="2">For issuance</option>
                                             <option value="3">On Process</option>
-                                            {{-- <option value="4">Disapproved</option>--}}
+                                           {{-- <option value="4">Disapproved</option>--}}
                                         </select>
                                     </div>
                                 </div>
@@ -153,13 +156,13 @@
                     @csrf
                     <div class="modal-body">
                         <p id="paragraphEditClearanceCertificateStatus"></p>
-                        <input type="hidden" name="barangay_clearance_certificate_id" id="textEditClearanceCertificateStatusClearanceCertificateId">
-                        <input type="hidden" name="status" id="textEditClearanceCertificateStatus">
+                        <input type="hidden" name="barangay_clearance_certificate_id" placeholder="Clearance Certificate Id" id="textEditClearanceCertificateStatusClearanceCertificateId">
+                        <input type="hidden" name="status" placeholder="Status" id="textEditClearanceCertificateStatus">
                     </div>
                     
                     <div class="modal-footer justify-content-between">
                         <button type="button" class="btn btn-default" data-bs-dismiss="modal">Close</button>
-                        <button type="submit" id="buttonEditBarangayClearanceCertificateStatus" class="btn btn-primary"><i id="iconEditBarangayClearanceCertificateStatus" class="fa fa-check"></i> Save</button>
+                        <button type="submit" id="buttonEditClearanceCertificateStatus" class="btn btn-primary"><i id="iconEditClearanceCertificate" class="fa fa-check"></i> Save</button>
                     </div>
                 </form>
             </div>
@@ -167,108 +170,125 @@
     </div><!-- Edit ClearanceCertificate Status Modal End -->
 @endsection
 
-<!-- JS CONTENT -->
+<!--     {{-- JS CONTENT --}} -->
 @section('js_content')
-<script type="text/javascript">
-    $(document).ready(function () {
-        // Initialize Select2 Elements
-        $('.bootstrap-5').select2({
-            theme: 'bootstrap-5'
-        });
+    <script type="text/javascript">
+        $(document).ready(function () {
+            // Initialize Select2 Elements
+            // $('.bootstrap-5').select2();
+            $('.bootstrap-5').select2({
+                theme: 'bootstrap-5'
+            });
 
-        getResidentsBarangayClearance($('#selectResident'));
+            getResidentsBarangayClearance($('#selectResident'));
+            
+            $("#formAddBarangayClearanceCertificate").submit(function(event){
+                event.preventDefault();
+                addBarangayClearanceCertificate();
+            });
 
-        $("#formAddBarangayClearanceCertificate").submit(function(event){
-            event.preventDefault();
-            addBarangayClearanceCertificate();
-        });
-
-        const dataTablesBarangayClearanceCertificate = $("#tableBarangayClearanceCertificate").DataTable({
-            "processing": false,
-            "serverSide": true,
-            "responsive": true,
-            "orderClasses": false, // disable sorting_1 for unknown background
-            "ajax": {
-                url: "{{ route('view_barangay_clearance_certificate') }}",
-                dataSrc: function (json) {
-                    console.log(json); // Log the response data
-                    return json.data;
-                }
-            },
-            "columns": [
-                { "data": "action", orderable: false, searchable: false },
-                { "data": function(data){
-                    let firstName = data.resident_info?.user_info?.firstname || 'N/A';
-                    let lastName = data.resident_info?.user_info?.lastname || '';
-                    return capitalizeFirstLetter(firstName) + ' ' + capitalizeFirstLetter(lastName);
-                }},
-                { "data": function(data){
-                    return data.resident_info?.user_info?.gender || 'N/A';
-                }},
-                { "data": function(data){
-                    return data.resident_info?.user_info?.civil_status || 'N/A';
-                }},
-                { "data": "ticket_number" },
-                { "data": "ticket_datetime" },
-                { "data": "status" },
-            ],
-            "columnDefs": [
-                { className: 'align-middle', targets: [0, 1, 2, 3] },
-            ],
-            "createdRow": function(row, data, index) {
-                $('td', row).eq(1).css('white-space', 'normal');
-                $('td', row).eq(2).css('white-space', 'normal');
-            },
-        });
-
-        $(document).on('click', '.actionEditBarangayClearanceCertificate', function(){
-            let id = $(this).attr('barangay-clearance-certificate-id');
-            $("input[name='barangay_clearance_certificate_id']", $("#formAddBarangayClearanceCertificate")).val(id);
-            getBarangayClearanceCertificateById(id);
-        });
-
-        function getCedulaBasis() {
-            $.ajax({
-                url: "{{ route('get_issuance_certification') }}",
-                method: "get",
-                data: {
-                    id: 1, // 6-Cedula, 5-License & Permit, 4-Registration, 3-Residency, 2-Indigency, 1-Brgy. Clearance
+            dataTablesBarangayClearanceCertificate = $("#tableBarangayClearanceCertificate").DataTable({
+                "processing" : false,
+                "serverSide" : true,
+                "responsive": true,
+                "orderClasses": false, // disable sorting_1 for unknown background
+                // "order": [[ 0, "desc" ],[ 4, "desc" ]],
+                "ajax" : {
+                    url: "view_barangay_clearance_certificate",
                 },
-                dataType: "json",
-                success: function(response) {
-                    let issuanceConfigurationDetails = response['issuanceConfigurationDetails'];
-                    if(issuanceConfigurationDetails.length > 0) {
-                        let processingTime = issuanceConfigurationDetails[0].processing_time;
-                        let cedulaProcessingTime;
-                        switch(processingTime) {
-                            case 1: cedulaProcessingTime = '1 Day'; break;
-                            case 2: cedulaProcessingTime = '2 Days'; break;
-                            case 3: cedulaProcessingTime = '3 Days'; break;
-                            case 4: cedulaProcessingTime = '4 Days'; break;
-                            case 5: cedulaProcessingTime = '5 Days'; break;
-                            case 6: cedulaProcessingTime = '1 Week'; break;
-                            case 7: cedulaProcessingTime = '2 Weeks'; break;
-                            case 8: cedulaProcessingTime = '3 Weeks'; break;
-                            case 9: cedulaProcessingTime = '1 Month'; break;
-                            default: cedulaProcessingTime = 'Other'; break;
-                        }
-                        $("#issuanceConfigurationId").val(issuanceConfigurationDetails[0].id);
-                        $("#textTotalAmount").val(issuanceConfigurationDetails[0].amount);
-                        $("#textProcessingTime").val(cedulaProcessingTime);
-                    } else {
-                        toastr.warning('No records found!');
-                    }
-                },
-                error: function(data, xhr, status) {
-                    toastr.error('An error occurred!\n' + 'Data: ' + JSON.stringify(data) + "\n" + "XHR: " + xhr + "\n" + "Status: " + status);
+                "columns":[
+                    { "data" : "action", orderable:false, searchable:false},
+                    { "data" : function(data){
+                        return capitalizeFirstLetter(data.resident_info.user_info.firstname) + ' ' +capitalizeFirstLetter(data.resident_info.user_info.lastname);
+                    }},
+                    { "data" : "gender"},
+                    { "data" : "civil_status"},
+                    { "data" : "ticket_number"},
+                    { "data" : "ticket_datetime"},
+                    { "data" : "status"},
+                ],
+                "columnDefs": [
+                    { className: 'align-middle', targets: [0, 1, 2, 3] },
+                ],
+                "createdRow": function(row, data, index) {
+                    $('td', row).eq(1).css('white-space', 'normal');
+                    $('td', row).eq(2).css('white-space', 'normal');
+                    // console.log('row ', row);
+                    // console.log('data ', data);
+                    // console.log('index ', index);
                 },
             });
-        }
 
-        $("#buttonAddModalBarangayClearanceCertificate").on('click', function(event){
-            event.preventDefault();
-            getCedulaBasis();
+            $(document).on('click', '.actionEditBarangayClearanceCertificate', function(){
+                let id = $(this).attr('barangay-clearance-certificate-id');
+                console.log('id ', id);
+                $("input[name='barangay_clearance_certificate_id'", $("#formAddBarangayClearanceCertificate")).val(id);
+                getBarangayClearanceCertificateById(id);
+            })
+
+            function getCedulaBasis(){
+                $.ajax({
+                    url: "get_issuance_certification",
+                    method: "get",
+                    data: {
+                        id: 1, // 6-Cedula, 5-License & Permit, 4-Registration, 3-Residency, 2-Indigency, 1-Brgy. Clearance
+                    },
+                    dataType: "json",
+                    success: function(response){
+                        let issuanceConfigurationDetails = response['issuanceConfigurationDetails'];
+                        if(issuanceConfigurationDetails.length > 0){
+                            let cedulaProcessingTime = "";
+                            if(issuanceConfigurationDetails[0].processing_time == 1){
+                                cedulaProcessingTime = '1 Day';
+                            }
+                            else if(issuanceConfigurationDetails[0].processing_time == 2){
+                                cedulaProcessingTime = '2 Days';
+                            }
+                            else if(issuanceConfigurationDetails[0].processing_time == 3){
+                                cedulaProcessingTime = '3 Days';
+                            }
+                            else if(issuanceConfigurationDetails[0].processing_time == 4){
+                                cedulaProcessingTime = '4 Days';
+                            }
+                            else if(issuanceConfigurationDetails[0].processing_time == 5){
+                                cedulaProcessingTime = '5 Days';
+                            }
+                            else if(issuanceConfigurationDetails[0].processing_time == 6){
+                                cedulaProcessingTime = '1 Week';
+                            }
+                            else if(issuanceConfigurationDetails[0].processing_time == 7){
+                                cedulaProcessingTime = '2 Weeks';
+                            }
+                            else if(issuanceConfigurationDetails[0].processing_time == 8){
+                                cedulaProcessingTime = '3 Weeks';
+                            }
+                            else if(issuanceConfigurationDetails[0].processing_time == 9){
+                                cedulaProcessingTime = '1 Month';
+                            }
+                            else{
+                                cedulaProcessingTime = 'Other';
+                            }
+                            $("#issuanceConfigurationId").val(issuanceConfigurationDetails[0].id);
+                            $("#textTotalAmount").val(issuanceConfigurationDetails[0].amount);
+                            $("#textProcessingTime").val(cedulaProcessingTime);
+                        }
+                        else{
+                            toastr.warning('No records found!');
+                        }
+                    },
+                    error: function(data, xhr, status){
+                        toastr.error('An error occured!\n' + 'Data: ' + data + "\n" + "XHR: " + xhr + "\n" + "Status: " + status);
+                    },
+                });
+            }
+
+            
+            $("#buttonAddModalBarangayClearanceCertificate").on('click',function(event){
+                event.preventDefault();
+                getCedulaBasis();
+                // console.log('userId ', $('#textUserId').val());
+                // getResidentForCedulaByUserId($('#textUserId').val());
+            });
         });
-    });
-</script>
+    </script>
 @endsection
